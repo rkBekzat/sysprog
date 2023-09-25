496dda836349cd35db33e1e2a13e2ae3f7f188f6
rkBekzat <bekzatrakhimbayev@gmail.com>
2023-09-25 23:48:24 +0300

    added latency

--- 1/solution.c
+++ 1/solution.c
@@ -17,14 +17,12 @@ struct my_context {
     int size;
     bool sorted;
 
-    uint total_time;
-
-	/** ADD HERE YOUR OWN MEMBERS, SUCH AS FILE NAME, WORK TIME, ... */
 };
 
 struct works{
     struct my_context *files;
     int sz;
+    uint lat;
 };
 
 static struct my_context *
@@ -34,24 +32,27 @@ my_context_new(const char *filename)
 	ctx->filename = strdup(filename);
     ctx->size = 0;
 	ctx->arr = NULL;
-    ctx->total_time = 0;
     ctx->sorted = false;
     return ctx;
 }
 
-static struct  works * works_new(int size){
+static struct  works * works_new(int size, int lat){
     struct works * result = malloc(sizeof (*result));
     result->files = (struct my_context *)malloc(sizeof (struct my_context)*size);
     result->sz = size;
+    result->lat = (uint)lat;
     return result;
 }
 
-//static void
-//my_context_delete(struct my_context *ctx)
-//{
-//	free(ctx->filename);
-//    free(ctx);
-//}
+static void
+works_delete(struct works *ctx)
+{
+    for(int i = 0; i < ctx->sz; i++){
+        free(ctx->files[i].arr);
+        free(ctx->files[i].filename);
+    }
+    free(ctx);
+}
 
 
 static void assign(int *arr, int *p1, int *cur, int *p2){
@@ -96,20 +97,22 @@ sorting(int *arr, int l, int r) {
     sorting(arr, mid+1, r);
 
     merge(arr, l, r);
-
 }
 
-/**
- * Coroutine body. This code is executed by all the coroutines. Here you
- * implement your solution, sort each individual file.
- */
+
+uint microtimes() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    uint s = (uint) ts.tv_sec*100000+(uint)ts.tv_nsec/1000;
+    return s;
+}
+
+
 static int
 coroutine_func_f(void *context)
 {
-	/* IMPLEMENT SORTING OF INDIVIDUAL FILES HERE. */
-
-//    struct coro *this = coro_this();
     struct works *ctx = context;
+    uint last_update = microtimes();
     for(int i = 0; i < ctx->sz; i++){
         if(ctx->files[i].sorted){
             continue;
@@ -124,10 +127,12 @@ coroutine_func_f(void *context)
             ctx->files[i].arr = (int *) realloc(ctx->files[i].arr, sizeof (int) * ctx->files[i].size);
             ctx->files[i].arr[ctx->files[i].size-1] = next;
         }
-
         sorting(ctx->files[i].arr, 0, ctx->files[i].size);
         ctx->files[i].sorted = true;
-        coro_yield();
+        if(microtimes()-last_update >= ctx->lat) {
+            coro_yield();
+            last_update = microtimes();
+        }
     }
 	return 0;
 }
@@ -156,79 +161,56 @@ struct my_context  combine(struct my_context * first, struct my_context *second)
     return *answer;
 }
 
-uint microtimes() {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    uint s = (uint) ts.tv_sec*100000+(uint)ts.tv_nsec/1000;
-    return s;
-}
-
 int
 main(int argc, char **argv)
 {
     if(argc == 1) {
         exit(0);
     }
-    if(argc < 3){
-        printf("STARNGE\n");
-        fprintf(stderr, "Usage: %s COROUTINES FILE...", argv[0]);
+    if(argc < 4){
+        fprintf(stderr, "Usage: %s LATENCY COROUTINES FILE...", argv[0]);
         exit(1);
     }
-    int num_cor = 1;
-    if(!sscanf(argv[1], "%d", &num_cor) || num_cor < 1){
-        printf("VERY STARNGE\n");
+
+    int latency;
+    if(!sscanf(argv[1], "%d", &latency) || latency < 1){
+        fprintf(stderr, "LATENCY should be natural");
+        exit(1);
+    }
+
+    int num_cor;
+    if(!sscanf(argv[2], "%d", &num_cor) || num_cor < 1){
         fprintf(stderr, "COROUTINES should be natural");
         exit(1);
     }
-	/* Initialize our coroutine global cooperative scheduler. */
-	coro_sched_init();
 
+    coro_sched_init();
     uint start = microtimes();
 
-
-    int num_file = argc - 2;
-    struct works * data = works_new(num_file);
+    int num_file = argc - 3;
+    struct works * data = works_new(num_file, latency);
 
     for(int i = 0; i < num_file; i++){
-        data->files[i] = *my_context_new(argv[i+2]);
+        data->files[i] = *my_context_new(argv[i+3]);
     }
-	/* Start several coroutines. */
     for (int i = 0; i < num_cor; ++i) {
-		/*
-		 * The coroutines can take any 'void *' interpretation of which
-		 * depends on what you want. Here as an example I give them
-		 * some names.
-		 */
-		/*
-		 * I have to copy the name. Otherwise all the coroutines would
-		 * have the same name when they finally start.
-		 */
 		coro_new(coroutine_func_f, data);
     }
-	/* Wait for all the coroutines to end. */
-	struct coro *c;
+    struct coro *c;
 	while ((c = coro_sched_wait()) != NULL) {
-		/*
-		 * Each 'wait' returns a finished coroutine with which you can
-		 * do anything you want. Like check its exit status, for
-		 * example. Don't forget to free the coroutine afterwards.
-		 */
 		printf("Finished %d\n", coro_status(c));
 		coro_delete(c);
 	}
-	/* All coroutines have finished. */
-
-	/* IMPLEMENT MERGING OF THE SORTED ARRAYS HERE. */
-
     for(int i = 1; i < num_file; i++){
-
         data->files[i] = combine(&data->files[i], &data->files[i-1]);
     }
+
     FILE *result = fopen(data->files[num_file-1].filename, "w");
     for(int i = 0; i < data->files[num_file-1].size; i++){
         fprintf(result, "%d ", data->files[num_file-1].arr[i]);
     }
-    free(data);
+
+    works_delete(data);
     fclose(result);
 
     uint end = microtimes();
